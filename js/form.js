26972f1b3dcfd8c3117c2510347db019873d7d56
redoyrahman <aanshakil@gmail.com>
2019-04-12 19:17:20 +0600

    waka

---
+++ js/form.js
@@ -0,0 +1,15 @@
+function check(){
+    var user=["shakil"];
+    var pass=["shakil123"];
+    var username= document.getElementById('username').value;
+    var password= document.getElementById('password').value;
+
+    if(user[0] === username && pass[0] === password){
+        return true;
+    }
+    else {
+        alert("agfuya");
+        return false;
+    }
+}
+
